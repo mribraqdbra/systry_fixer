4baf300ffd97ff293e8b0166ce9ac9a10a8672d3
mribraqdbra <30726221+mribraqdbra@users.noreply.github.com>
2019-07-22 20:41:00 +0300

    Create SysTry_Fixer_v1.0.bat

---
+++ SysTry_Fixer_v1.0.bat
@@ -0,0 +1,18 @@
+@echo off
+title SysTry Fixer v1.0
+color 0a
+mode con: cols=25 lines=5
+echo.
+echo  SysTry Fixer v1.0
+timeout /t 2 /nobreak >nul
+cls
+echo.
+echo  Fixing...
+timeout /t 2 /nobreak >nul
+tskill explorer >nul
+cls
+echo.
+echo  Enjoy!
+timeout /t 2 /nobreak >nul
+exit /b
+::Author: @mribraqdbra
